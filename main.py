71bae56f2ba06e3d85d4013a3ed3ea3d67ff3621
Lukas-Buehlmann <157663483+Lukas-Buehlmann@users.noreply.github.com>
2024-03-02 21:07:53 -0500

    a lot was done. I'm not writing it all out because this is a private program and I'm collaborating with myself

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import sys
 import math
 import random
 
-KERNEL_SIZE = 5
+KERNEL_SIZE = 20
+
+WIDTH = 512
+HEIGHT = 512
 
 
 # a class to isolate a given colour and apply effects to it
@@ -13,6 +16,7 @@ class Colour:
     def __init__(self, h, sens, name, data_hsv):
         self.mask = None
         self.name = name
+        self.origin_h = h
         self.h = h
 
         # catches hue values that are too low
@@ -36,16 +40,21 @@ class Colour:
     # defines the shape of found objects and draws rectangles representing the bounding box
     def get_contour(self, frame_data):
         contours, hierarchy = cv2.findContours(self.mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        colour = self.hsv_to_bgr((self.origin_h, 255, 255))
+
+        rect_list = [colour]
 
         for pic, contour in enumerate(contours):
             area = cv2.contourArea(contour)
             if area > 300:
                 x, y, w, h = cv2.boundingRect(contour)
+                rect_list.append(pygame.Rect(x, y, w, h))
+
                 frame_data = cv2.rectangle(
                     frame_data,
                     (x, y),
                     (x + w, y + h),
-                    self.hsv_to_bgr((self.h, 255, 255)),
+                    colour,
                     2
                 )
 
@@ -54,20 +63,24 @@ class Colour:
                     f"{self.name} Colour",
                     (x, y),
                     cv2.FONT_HERSHEY_SIMPLEX, 1.0,
-                    self.hsv_to_bgr((self.h, 255, 255))
+                    colour
                 )
 
-        return frame_data
+        return frame_data, rect_list
 
     @staticmethod
     def hsv_to_bgr(hsv):
         try:
             h, s, v = hsv
 
+            # print(f"h={h} s={s} v={v}")
+
             # formula uses s and v as values from 0-1 so dividing by 255^2 maps them properly
             c = (v * s) / (255**2)  # 1 when v and s are 255
             new_h = h / 30  # maps between 0 and 6
             x = c * (1 - math.fabs(new_h % 2 - 1))
+
+            # print(f"new_h={new_h} c={c} x={x}")
 
             if 0 <= new_h < 1:
                 temp = (0, x, c)
@@ -86,7 +99,7 @@ class Colour:
                 return (0, 0, 0)
 
             m = v - c*255
-            print((temp[0]*255 + m, temp[1]*255 + m, temp[2]*255 + m))
+            # print((temp[0]*255 + m, temp[1]*255 + m, temp[2]*255 + m))
             return (temp[0]*255 + m, temp[1]*255 + m, temp[2]*255 + m)
 
         except UnboundLocalError:
@@ -96,40 +109,101 @@ class Colour:
             print(f"data entered: h={h} s={s} v={v}")
             return (0, 0, 0)
 
+    @staticmethod
+    def bgr_to_rgb(bgr):
+        return bgr[2], bgr[1], bgr[0]
+
+
+class Ball:
+    def __init__(self, pos, r, colour):
+        self.x, self.y = pos
+        self.rad = r
+        self.colour = colour
+
+    def update(self):
+        self.rad -= 0.1
+
+    def draw(self, surface):
+        pygame.draw.circle(surface, self.colour, (self.x, self.y), self.rad)
+
 
 def main():
+
+    pygame.init()
+
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    clock = pygame.time.Clock()
+
+    balls = []
 
     # creates an object that holds default device camera values
     camera = cv2.VideoCapture(0)
 
+    framerate = camera.get(cv2.CAP_PROP_FPS)
+
     while True:
+
+        # CV2 Process---------------------------------------------------------------------------------------------------
+        rects = []
 
         # grabs the frame data
         frame = camera.read()[1]
+        frame = cv2.flip(frame, 1)
 
         # convert rgb to hsv
         hsv_data = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        # Set up lower and upper bounds for each desired colour---------------------------------------------------------
-        red = Colour(0, 20, "red", hsv_data)
+        # Set up lower and upper bounds for each desired colour
+        red = Colour(0, 10, "red", hsv_data)
         red_result = red.dilate_colour(KERNEL_SIZE, frame)
 
-        green = Colour(60, 10, "green", hsv_data)
+        green = Colour(60, 20, "green", hsv_data)
         green_result = green.dilate_colour(KERNEL_SIZE, frame)
 
         blue = Colour(120, 20, "blue", hsv_data)
         blue_result = blue.dilate_colour(KERNEL_SIZE, frame)
 
-        frame = red.get_contour(frame)
-        frame = green.get_contour(frame)
-        frame = blue.get_contour(frame)
+        # finds the location of all patches of the given colours. Stores in a list of rectangles
+        # frame, temp_rects = red.get_contour(frame)
+        # rects.append(temp_rects)
+        frame, temp_rects = green.get_contour(frame)
+        rects.append(temp_rects)
+        frame, temp = blue.get_contour(frame)
+        rects.append(temp_rects)
 
-        # Program Termination
-        cv2.imshow("Multiple Color Detection in Real-Time", frame)
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            camera.release()
-            cv2.destroyAllWindows()
-            break
+        cv2.imshow("Colour Detection Viewer", frame)
+        # End of CV2 Process--------------------------------------------------------------------------------------------
+
+        # Start of Pygame Process---------------------------------------------------------------------------------------
+        clock.tick(framerate)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                camera.release()
+                pygame.quit()
+                cv2.destroyAllWindows()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    camera.release()
+                    pygame.quit()
+                    cv2.destroyAllWindows()
+                    sys.exit()
+
+        screen.fill((0, 0, 0))
+
+        for colour in rects:
+            for i in range(1, len(colour)):
+                balls.append(Ball(colour[i].center, 8, Colour.bgr_to_rgb(colour[0])))
+
+        for ball in balls:
+            ball.update()
+            ball.draw(screen)
+
+        pygame.display.flip()
+
+        # End of Pygame Process-----------------------------------------------------------------------------------------
 
 
 if __name__ == "__main__":
